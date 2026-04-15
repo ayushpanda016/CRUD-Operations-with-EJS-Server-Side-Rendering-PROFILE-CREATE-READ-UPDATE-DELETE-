bd9346cb118894bc7e2fc6e771640e92ac733c95
ayush panda <ayushpanda016@gmail.com>
2026-04-16 04:35:16 +0530

    added Update features by using edit user

--- app.js
+++ app.js
@@ -21,6 +21,17 @@ app.get('/read', async (req,res)=> {
    res.render("read", {users: alluser});
 })
 
+app.get('/edit/:userid', async (req,res)=> {
+   let user = await usermodel.findOne({_id: req.params.userid});
+   res.render("edit", {user});
+})
+
+app.post('/update/:userid', async (req,res)=> {
+   let {image, name, email} = req.body;
+   let user = await usermodel.findOneAndUpdate({_id: req.params.userid},{image, name, email}, {new:true});
+   res.redirect("/read");
+})
+
 app.get('/delete/:id', async (req,res)=> {
    let alluser = await usermodel.findOneAndDelete({_id: req.params.id});
    res.redirect("/read");
